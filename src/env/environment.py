e0ba6894dc0257815a94fd28fc3762bb255de17a
Lilian Biscarrat <lilian.bscr@gmail.com>
2023-01-06 15:42:09 +0100

    environment

---
+++ src/env/environment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  6 15:37:12 2023
+
+@author: lilia
+"""
+
+
+
+class Environment():
+    """
+    Implementation of environment
+    """
+
+    def __init__(self, args):
+        super().__init__(args)
+
+    def any_common_method(self, args):
+        raise Exception("Not implemented")
